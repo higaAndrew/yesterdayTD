4d65825648d63d6f45afc262f36059d81022526e
Andrew Higa <higa.andrew@csu.fullerton.edu>
2024-06-15 02:45:07 -0700

    modified the spawn/despawn point script modified it so that it must be the child of the enemy's hitbox, just to organize it a bit

--- YesterdayTD/#debug/spawn_despawn_point_preview.gd
+++ YesterdayTD/#debug/spawn_despawn_point_preview.gd
@@ -1,7 +1,8 @@
 @tool
+class_name SpawnDespawnPointPreview
 extends Node
+## ONLY USE AS CHILD OF ENEMY HITBOX
 
-@export var hitbox: CollisionShape2D
 @export var spawn_point: Marker2D
 @export var despawn_point: Area2D
 
@@ -9,5 +10,5 @@ extends Node
 ## see the range and sightline update in the editor
 func _physics_process(_delta) -> void:
 	if Engine.is_editor_hint():
-		spawn_point.position = Vector2(hitbox.shape.radius, 0)
-		despawn_point.position = Vector2(-hitbox.shape.radius, 0)
+		spawn_point.position = Vector2(get_parent().shape.radius, 0)
+		despawn_point.position = Vector2(-get_parent().shape.radius, 0)
